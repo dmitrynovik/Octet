82092bbbba7e88af70a06e964d7619665bc51d8b
dmitrynovik <cryinstone@gmail.com>
2015-10-21 23:01:25 +1100

    QUnit

---
+++ Octet.Javascript/UnitTest.js
@@ -0,0 +1,38 @@
+﻿
+QUnit.test("'a' startsWith 'a'", function (assert) {
+    assert.ok("a".startsWith("a"));
+});
+
+QUnit.test("'a' startsWith empty string", function (assert) {
+    assert.ok("a".startsWith(""));
+});
+
+QUnit.test("'a' startsWith null", function (assert) {
+    assert.ok("a".startsWith(null));
+});
+
+QUnit.test("'ab' does not start with 'b'", function (assert) {
+    assert.notOk("a".startsWith("b"));
+});
+
+QUnit.test("'ab' endsWith 'b'", function (assert) {
+    assert.ok("ab".endsWith("b"));
+});
+
+QUnit.test("'a' endsWith empty string", function (assert) {
+    assert.ok("a".endsWith(""));
+});
+
+QUnit.test("'a' endsWith null", function (assert) {
+    assert.ok("a".endsWith(null));
+});
+
+QUnit.test("'ab' does not end with 'a'", function (assert) {
+    assert.notOk("ab".endsWith("a"));
+});
+
+QUnit.test("strip Html", function (assert) {
+    assert.ok("<head><meta charset='utf-'><title>QUnit Example</title></head>".stripHtml() === "QUnit Example");
+});
+
+
